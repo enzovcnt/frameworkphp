1d1432cca773eb174fb97b64d366a6cf9d954003
Enzo <enzovcnt@gmail.com>
2025-10-21 18:42:27 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'esdlyonb2-2526/frameworkphp',
+        'pretty_version' => '0.3',
+        'version' => '0.3.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'esdlyonb2-2526/frameworkphp' => array(
+            'pretty_version' => '0.3',
+            'version' => '0.3.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
