2d7f3b5f718f756bb6f3641595c18c3b27146bcb
Enzo <enzovcnt@gmail.com>
2025-10-21 19:43:08 +0200

    create user repository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Attributes\TargetEntity;
+use Core\Repository\Repository;
+
+
+#[TargetEntity(entityName: User::class)]
+class UserRepository extends Repository
+{
+
+    public function save(User $user): int
+    {
+        $this->pdo->prepare("INSERT INTO $this->tableName (email, password) VALUES (:email, :password)")
+            ->execute([
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword(),
+            ]);
+
+        return $this->pdo->lastInsertId();
+    }
+
+}
