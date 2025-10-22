0e5f65574e9f91332745b6b77a0def5e006c8cdc
Enzo <enzovcnt@gmail.com>
2025-10-22 02:37:00 +0200

    create registration form to enter the user in the database

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form;
+
+use Core\Form\FormParam;
+use Core\Form\FormType;
+
+class RegistrationType extends FormType
+{
+
+    public function __construct()
+    {
+        $this->build();
+    }
+
+    public function build()
+    {
+        $this->add(new FormParam("email", "text"));
+        $this->add(new FormParam("password", "text"));
+    }
+
+}
